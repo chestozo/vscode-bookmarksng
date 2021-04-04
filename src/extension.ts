14f1ae710ebbffbcd3557309b38136fae98d5a58
chestozo <romanok@bk.ru>
2021-04-05 00:00:34 +0300

    Fixes #7
    - add custom icon color in settings

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,11 @@
 import * as vscode from 'vscode'
 import { bookmarksManager } from './bookmarks'
 
+const DEFAULT_COLOR = '#333'
+
+const getIconContents = (iconColor: string) =>
+  `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="${iconColor}"><path d="M7 5v23l1.594-1.188L16 21.25l7.406 5.563L25 28V5H7zm2 2h14v17l-6.406-4.813L16 18.75l-.594.438L9 24V7z"/></svg>`
+
 export function activate(context: vscode.ExtensionContext) {
   bookmarksManager.init(context)
 
@@ -47,6 +52,25 @@ export function activate(context: vscode.ExtensionContext) {
   // @see https://github.com/microsoft/vscode/issues/54147
   vscode.workspace.onDidChangeTextDocument((event) => {
     bookmarksManager.handleTextChanges(context, event.contentChanges)
+  })
+
+  vscode.workspace.onDidChangeConfiguration(async (evt) => {
+    const iconColor = vscode.workspace
+      .getConfiguration('bookmarksNG')
+      .get<string>('iconColor', '#333')
+
+    if (iconColor !== DEFAULT_COLOR) {
+      await vscode.workspace.fs.writeFile(
+        vscode.Uri.parse(context.asAbsolutePath('images/icon.svg')),
+        Buffer.from(getIconContents(iconColor))
+      )
+      await vscode.workspace.fs.writeFile(
+        vscode.Uri.parse(context.asAbsolutePath('images/icond.svg')),
+        Buffer.from(getIconContents(iconColor))
+      )
+
+      vscode.commands.executeCommand('workbench.action.reloadWindow')
+    }
   })
 }
 
